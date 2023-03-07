48f28073b310fe9d3ca0e1b84efff6b2e37ef67f
Klimenty <klimentykk@gmail.com>
2023-03-07 09:57:20 +0400

    husky hooks installed

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,19 @@
 import express from 'express'
 import { Request, Response } from 'express'
 import { readFile } from 'node:fs/promises'
+import { setup_env_vars } from './utils/env'
+
+const envs = new Map([
+	['MODE', 'dev'],
+	['FOO', 'bar'],
+])
+
+setup_env_vars(envs)
+
 const app = express()
 const port = 30042
+
+console.log(process.env.MODE)
 
 app.get('/', async (_: Request, res: Response) => {
 	res.send(
@@ -11,6 +22,7 @@ app.get('/', async (_: Request, res: Response) => {
 		}),
 	)
 })
+
 app.listen(port, () =>
 	console.log(`Example app listening on http://localhost:${port}`),
 )
