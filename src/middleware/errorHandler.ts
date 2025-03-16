3beb46ecf6d2fb1f8cc26c17d315a4ecb4d0dad7
Klimenty Karavaev <klimentykk@gmail.com>
2025-03-16 11:50:03 +0400

    delete unused code

--- src/middleware/errorHandler.ts
+++
@@ -1,7 +0,0 @@
-import type { ErrorRequestHandler } from 'express'
-
-export const globalErrorHandler: ErrorRequestHandler = (err: Error, _, res) => {
-	console.log(err.stack)
-	console.log(err.name, err.message)
-	res.status(500).json({ msg: 'Internal server error' })
-}
